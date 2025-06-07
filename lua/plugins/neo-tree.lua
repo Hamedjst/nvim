35865329fc910501fa1faa75e1b98d42d79b50d1
Hamed <hamed@debian.Development.com>
2025-06-07 19:54:07 +0330

    fixing nvim-web plugin

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,8 +1,7 @@
- return {
+return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
     dependencies = {
-      "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim"},
    config = function() 
      vim.keymap.set('n' , '<C-n>', ':Neotree filesystem reveal left<CR>', { desc = 'Starts Neotree'})
